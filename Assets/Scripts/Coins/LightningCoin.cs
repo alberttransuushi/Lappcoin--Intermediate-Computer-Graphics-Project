157c93cdfbf5feeaa656de0254cd4a33c9bb1014
Void-Foxx <ryan.h0386@gmail.com>
2023-01-19 12:24:22 -0500

    moar coin
    shadow coin and lightning coin done

--- Assets/Scripts/Coins/LightningCoin.cs
+++ Assets/Scripts/Coins/LightningCoin.cs
@@ -1,23 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-public class ShadowCoin : Coin
+public class LightningCoin : Coin
 {
-  [SerializeField] GameObject shadow;
+  [SerializeField] GameObject lightning;
   [SerializeField] private LayerMask layermask;
+
+  Vector3 point;
   private void Update() {
     Ray ray = new Ray(transform.position, Vector3.down);
     RaycastHit hit;
 
     if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
-      shadow.transform.position = hit.point;
+      point = hit.point;
     }
   }
   public override void HitCoin() {
-    GameObject player = GameObject.Find("Player");
-    player.transform.position = shadow.transform.position + Vector3.up;
+    Instantiate(lightning, point, Quaternion.Euler(Vector3.forward));
     Destroy(this.gameObject);
   }
 
